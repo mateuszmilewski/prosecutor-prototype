3d0345fc0f65daab78e013785f653f657ac007ca
Mateusz Milewski <milewski.88@gmail.com>
2019-06-19 12:11:49 +0200

    june-setup-with-ng-js
    june-setup-with-ng-js - almost ready - to check

---
+++ src/js/NEWS.js
@@ -0,0 +1,206 @@
+var G_NEWS = [
+
+    {
+        title: "Koncert, Tychy, Underground",
+        d: "20.07.2019",
+        img: ["destroyers2019.JPG"],
+        link: "https://www.facebook.com/events/2316215151970971/",
+        typ: "koncert",
+        widoczne: true
+    },
+    {
+        title: "Prosecutor nagrywa płytę",
+        img: ["nagrywanie01.JPG", "nagrywanie02.JPG", "nagrywanie03.JPG", "nagrywanie04.JPG"],
+        link: "",
+        desc: "Zespół Prosecutor wszedł do studia No Fear Records, by zarejestrować swoją drugą płytę. Wkrótce podamy więcej informacji!",
+        typ: "general",
+        widoczne: true
+    },
+    {
+        title: "METAL REVOLT, Wywiad.",
+        img: ["mr.JPG"],
+        desc: "",
+        link: "https://www.facebook.com/notes/metal-revolt/wywiad-5/219210181978630/",
+        typ: "general",
+        widoczne: true
+    },
+    {
+        title: "PODSUMOWANIE 2017",
+        img: ["podsumowanie2017.JPG"],
+        link: "",
+        desc: "PROSECUTOR - 2017 - zagraliśmy sporo koncertów (m.in w Czechach), ukazała się reedycja \"Krew Czarnej Ziemi\" na CD, trafiliśmy do Metal Hammer'a, w związku z tym chcielibyśmy serdecznie podziękować Wszystkim tym, którzy w większym lub mniejszym stopniu przyczynili się do tego, że był to dla nas bardzo udany rok. Przede wszystkim naszym sponsorom: firma transportowa IVA-TECH Ireneusz Chojnacki z Chorzowa, Studio tatuażu \"Angel\" - Rudy Śląskiej oraz drukarnia \"Matura\" z Bytomia dzięki którym w znaczny stopniu mogliśmy realizować nasze cele. Wielkie dzięki dla Leszka i jego Thrashing Madness, Rafał Monastyrski - Metal Hammer, Paweł Mielczarek - Antidotum Rock'n'Rollowe, Radio Era - Kovadło, Radio Revolta, Rzeźnia, Szczygieł Łukasz - wsparcie graficzne, Dziemba Grzegorz. Dziękujemy i pozdrawiamy wszystkie zespoły z którymi mieliśmy przyjemność zagrać m.in. DRAGON, QUO VADIS, SYNDROM, INDICIUM... Wszystkim (których wymieniliśmy i tych których nie, ale o nich pamiętamy), a także naszym fanom życzymy zdrowych, radosnych i spokojnych świąt oraz Szczęśliwego Nowego Roku, oby był dla Was tak udany lub jeszcze bardziej jak ten 2017 dla nas.Pozdrawiamy:Harry,Forrest,Dziadu,Pepko - zespół PROSECUTOR.",
+        typ: "general",
+        widoczne: false
+
+    },
+    {
+        title: "Koncert, Warszawa",
+        d: "09.12.2017",
+        img: ["wwa2017.JPG"],
+        link: "https://www.facebook.com/events/814247325445063/",
+        typ: "koncert",
+        widoczne: false
+    },
+    {
+        title: "GARAGE, Kraków",
+        d: "01.12.2017",
+        typ: "koncert",
+        widoczne: false
+    },
+    {
+        title: "OZI, Żory",
+        d: "24.11.2017",
+        typ: "koncert",
+        widoczne: false
+    },
+    {
+        title: "KAFLOWY MK, Nowa Sól ",
+        d: "18.11.2017",
+        typ: "koncert",
+        widoczne: false
+    },
+    {
+        title: "ELEKTROWNIA, Żagań ",
+        d: "17.11.2017",
+        typ: "koncert",
+        widoczne: false
+    },
+    {
+        title: "LIVERPOOL, Wrocław ",
+        d: "20.10.2017",
+        typ: "koncert",
+        widoczne: false
+    },
+    {
+        title: "TAWERNA, Tychy ",
+        d: "30.09.2017",
+        typ: "koncert",
+        widoczne: false
+    },
+    {
+        title: "ZAMKOWY, Racibórz ",
+        d: "16.09.2017",
+        typ: "koncert",
+        widoczne: false
+    },
+    {
+        title: "HARRYFEST, Baska, Czechy ",
+        d: "29.07.2017",
+        typ: "koncert",
+        widoczne: false
+    },
+    {
+        title: "MIASTO, Bielsko-Biała ",
+        d: "21.04.2017",
+        typ: "koncert",
+        widoczne: false
+    },
+    {
+        title: "Red and Black, Chorzów ",
+        d: "01.04.2017",
+        typ: "koncert",
+        widoczne: false
+    },
+    {
+        title: "ROCKa, Gliwice ",
+        d: "17.03.2017",
+        typ: "koncert",
+        widoczne: false
+    },
+    {
+        title: "MIASTO, Bielsko-Biała ",
+        d: "16.03.2017",
+        typ: "koncert",
+        widoczne: false
+    },
+    {
+        title: "Bytom, Gotyk ",
+        d: "04.03.2017",
+        typ: "koncert",
+        widoczne: false
+    },
+    {
+        title: "Red & Black, Chorzów ",
+        d: "03.12.2016",
+        typ: "koncert",
+        widoczne: false
+    },
+    {
+        title: "Red and Black, Chorzów ",
+        d: "01.04.2017",
+        typ: "koncert",
+        widoczne: false
+    },
+    {
+        title: "Arena, Olkusz ",
+        d: "26.11.2016",
+        typ: "koncert",
+        widoczne: false
+    },
+    {
+        title: "Eliminacje Rokosz Festiwal 2017, Bielsko-Biała ",
+        d: "16.11.2016",
+        typ: "koncert",
+        widoczne: false
+    },
+    {
+        title: "Koncert w Tawernie, Tychy ",
+        d: "29.10.2016",
+        typ: "koncert",
+        widoczne: false
+    },
+    {
+        title: "Thrash Metal Night z Rethorn i Section Denied w klubie Chillout, Zawiercie ",
+        d: "04.06.2016",
+        typ: "koncert",
+        widoczne: false
+    },
+    {
+        title: "Pub pod Ziemią, Kraków ",
+        d: "21.05 .2016",
+        typ: "koncert",
+        widoczne: false
+    },
+    {
+        title: "Rock Out, Dąbrowa Górnicza ",
+        d: "24.04.2016",
+        typ: "koncert",
+        widoczne: false
+    },
+    {
+        title: "Korba, Katowice ",
+        d: "26.03.2016",
+        typ: "koncert",
+        widoczne: false
+    },
+    {
+        title: "Klub Piekarnia, Opole ",
+        d: "20.02.2016",
+        typ: "koncert",
+        widoczne: false
+    },
+    {
+        title: "WALENTYN THRASH - Klub Gotyk, Bytom ",
+        d: "13.02.2016",
+        typ: "koncert",
+        widoczne: false
+    },
+    {
+        title: "Pub Alternatywa, Sosnowiec ",
+        d: "06.02.2016",
+        typ: "koncert",
+        widoczne: false
+    },
+    {
+        title: "LOUD RITUAL - Pub Czerwona Cegla",
+        d: "19.12.2015",
+        typ: "koncert",
+        widoczne: false
+    },
+    {
+        title: "Piekary Śląskie Oś.K. Andaluzja z Alcoholica, Ossa",
+        d: "16.02.2012",
+        typ: "koncert",
+        widoczne: false
+    }
+];
---
+++ src/js/SKLEP.js
@@ -0,0 +1,21 @@
+var G_SKLEP = [
+
+    {
+        title: "Krew Czarnej Ziemi na CD!",
+        img: ["kcz.JPG"],
+        link: "",
+        desc: "Zachęcamy naszych fanów do kupowania, a tym samym do wspierania naszego zespołu."
+    },
+    {
+        title: "Koszulki dostępne!",
+        img: ["tshirt.JPG"],
+        link: "",
+        desc: "Dostępne są rozmiary S,L,M,XL."
+    },
+    {
+        title: "Singiel",
+        img: ["singiel.JPG"],
+        link: "",
+        desc: "Dostępny jest wciąż singiel!"
+    }
+];
